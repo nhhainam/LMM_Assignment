b588b591e505aa6593a3dad776eacfef874ff63b
nhhainam <nhhainam@gmail.com>
2023-03-27 09:04:57 +0700

    Implement Register

--- LMMWebAPI/DataAccess/LmmAssignmentContext.cs
+++ LMMWebAPI/DataAccess/LmmAssignmentContext.cs
@@ -30,9 +30,14 @@ public partial class LmmAssignmentContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=HAINAM\\SQLEXPRESS; database =LMM_Assignment; uid=sa;pwd=sa;TrustServerCertificate=True;");
-
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string constr = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().
+                                GetConnectionString("MyDb").ToString();
+            optionsBuilder.UseSqlServer(constr);
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Assignment>(entity =>
